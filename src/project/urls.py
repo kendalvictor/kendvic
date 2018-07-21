8c287070706e73180870a1a9be2c7f7bca869b59
vvillacorta <v.villacorta.unmsm@gmail.com>
2018-07-21 17:54:19 -0500

    FIX setting remove DEBUG TOOLBAR

--- src/project/urls.py
+++ src/project/urls.py
@@ -9,10 +9,7 @@ urlpatterns = []
 
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        path(r'__debug__/', include(debug_toolbar.urls)),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 urlpatterns += [
