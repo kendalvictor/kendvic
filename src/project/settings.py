bc38a3509eb7db90d56496bd03e559f2d273c482
vvillacorta <v.villacorta.unmsm@gmail.com>
2018-07-21 18:29:10 -0500

    Revert "SETEAR db"
    This reverts commit 3407e472b4e6b151485b93b3f3a2997f3a50b853.

--- src/project/settings.py
+++ src/project/settings.py
@@ -84,15 +84,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'kendvic',
-        'USER': 'kendal',
-        'PASSWORD': 'kendal',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
