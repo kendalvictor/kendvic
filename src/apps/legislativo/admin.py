32cedb70d0c8d89bfdfbbb7ca208da2bc454b5f0
vvillacorta <v.villacorta.unmsm@gmail.com>
2018-07-22 11:53:01 -0500

    quit only in inline

--- src/apps/legislativo/admin.py
+++ src/apps/legislativo/admin.py
@@ -44,7 +44,6 @@ class AnswerAdmin(admin.ModelAdmin):
 class AnswerInline(admin.TabularInline):
     model = Answer
     extra = 0
-    readonly_fields = ('text', 'approved', 'user', 'law', 'displeases', 'like')
 
 
 @admin.register(Questions)
